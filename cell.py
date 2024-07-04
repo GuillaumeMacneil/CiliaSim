8502c5fbe58f58044dc7efbe547f58b84f0e07b1
MetallicSquid <gcmacneil@icloud.com>
2024-07-04 20:32:32 +0100

    A good start to a spring model, with a simple annealing phase

--- cell.py
+++ cell.py
@@ -8,11 +8,35 @@ class Cell():
         self.neighbours = neighbours
         self.area = area
 
+    def calculate_attraction(self, cells: list):
+        total_attraction = np.array([0.0, 0.0])
+        position = np.array([self.x, self.y])
+        for neighbour in self.neighbours:
+            neighbour_position = np.array([cells[neighbour].x, cells[neighbour].y])
+            difference = neighbour_position - position
+            distance = np.linalg.norm(difference)
+            unit_direction_vector = difference / distance
+           
+            # The simplest possible spring set up
+            force = distance - 1 #+ (np.random.rand(1) * 0.0025)
+
+            total_attraction += force * unit_direction_vector
+
+        return total_attraction
+
+    def calculate_repulsion(self, cells: list):
+        total_repulsion = np.array([0, 0])
+
+        return total_repulsion
+
 class BasicCell(Cell):
-    pass
+    def step(self, cells: list):
+        self.x, self.y = [self.x, self.y] + 0.95 * 0.0025 * (self.calculate_attraction(cells) - self.calculate_repulsion(cells))
 
 class MulticiliatedCell(Cell):
-    pass
+    def step(self, cells: list):
+        self.x, self.y = [self.x, self.y] + 0.95 * 0.0025 * (self.calculate_attraction(cells) - self.calculate_repulsion(cells))
 
 class BorderCell(Cell):
-    pass
+    def step(self, cells: list):
+        pass
--- tissue.py
+++ tissue.py
@@ -1,11 +1,11 @@
 from cell import BasicCell, MulticiliatedCell, BorderCell
+from functions import *
 
 import numpy as np
-from scipy.spatial import Voronoi, voronoi_plot_2d
+from scipy.spatial import Voronoi
 from scipy.stats import qmc
 from collections import defaultdict
-import matplotlib.pyplot as plt # <- TESTING
-
+import matplotlib.pyplot as plt
 class Tissue():
     """
     Represents a tissue consisting of multiciliated and basic cells arranged in a 2D space.
@@ -22,7 +22,8 @@ class Tissue():
         Initializes a Tissue object with a given size and cilia density.
 
         Args:
-            size (int): The size of the tissue.
+            x (int): The size x-dimension of the tissue.
+            y (int): The size y-dimension of the tissue.
             cilia_density (float): The density of multiciliated cells.
         """
 
@@ -34,6 +35,10 @@ class Tissue():
         self.density = cilia_density
         self.num_cells = (x - 1) * (y - 1)
         self.cells = []
+        self.center_only = False
+
+    def set_center_only(self, value: bool):
+        self.center_only = value
 
     def generate_cells(self, points: np.ndarray):
         """
@@ -70,8 +75,8 @@ class Tissue():
         cell_data = []
         for i in range(len(points)):
             cell_data.append((points[i][0], points[i][1], np.array(neighbours[i]), areas[i]))
-
-        cell_type_mask = assign_cell_types(self.x, self.y, constrained_regions, constrained_vertices, cell_data, self.density)
+            
+        cell_type_mask = assign_cell_types(self.x, self.y, constrained_regions, constrained_vertices, cell_data, self.density, self.center_only)
         
         for i in range(len(cell_data)):
             if cell_type_mask[i] == 1:
@@ -81,7 +86,7 @@ class Tissue():
             else:
                 self.cells.append(BasicCell(i, *cell_data[i]))
         
-        self.cells = np.array(self.cells)
+        #self.cells = np.array(self.cells)
     
     def random_layout(self):
         """
@@ -122,14 +127,14 @@ class Tissue():
 
         self.generate_cells(np.array(points))
 
-    def render(self, title: str = ""):
+    def render(self, title: str = "", plot: tuple = (None, None)):
         """
         Renders the tissue with cells colored based on their type.
-        Multiciliated cells are orange, border cells are green, and basic cells are blue.
 
         Args:
             title (str): The title of the plot.
         """
+        
         multiciliated_points = []
         border_points = []
         basic_points = []
@@ -147,222 +152,48 @@ class Tissue():
 
         voronoi = Voronoi(points)
 
-        fig, ax = plt.subplots()
+        if not plot[0] and not plot[1]:
+            fig, ax = plt.subplots()
+        else:
+            fig, ax = plot
+            ax.clear()
 
         constrained_vertices, constrained_regions = constrain_voronoi(self.x, self.y, voronoi)
 
-        for region in constrained_regions:
-            points = constrained_vertices[region]
-            plt.fill(*zip(*points), edgecolor="black", fill=False)
+        for i in range(len(constrained_regions)):
+            polygon = constrained_vertices[constrained_regions[i]]
+            ax.fill(*zip(*polygon), edgecolor="black", fill=False)
+            
 
         basic_points = np.array(basic_points)
         multiciliated_points = np.array(multiciliated_points)
         border_points = np.array(border_points)
 
-        ax.scatter(basic_points[:, 0], basic_points[:, 1], s=20, color="blue")
+        #ax.scatter(basic_points[:, 0], basic_points[:, 1], s=20, color="blue")
         ax.scatter(multiciliated_points[:, 0], multiciliated_points[:, 1], s=20, color="orange")   
         ax.scatter(border_points[:, 0], border_points[:, 1], s=20, color="green")   
 
-        #ax.plot([0, self.x, self.x, 0, 0], [0, 0, self.y, self.y, 0], 'k-')
+        ax.plot([0, self.x, self.x, 0, 0], [0, 0, self.y, self.y, 0], 'k-')
 
         plt.title(title)
         plt.show()
+        plt.pause(0.1)
 
+        return (fig, ax)
 
-def polygon_area(vertices: np.ndarray):
-    """
-    Calculates the area of a polygon given its vertices using the shoelace formula.
+    def anneal(self, iterations: int = 2000):
+        plt.ion()
+        plot = (None, None)
+        for iteration in range(iterations):
+            for cell in self.cells:
+                cell.step(self.cells)
 
-    Args:
-        vertices (np.ndarray): The vertices of the polygon.
-
-    Returns:
-        float: The area of the polygon.
-    """
-    x = vertices[:, 0]
-    y = vertices[:, 1]
-
-    return 0.5 * np.abs(np.dot(x, np.roll(y, 1)) - np.dot(y, np.roll(x, 1)))
-
-def is_center(polygon_vertices: np.ndarray, candidates: np.ndarray) -> int:
-    distances = []
-    for candidate in candidates:
-        distances.append(np.sum(np.sum((polygon_vertices - candidate) ** 2, axis=1)))
-
-    return int(np.argmin(np.array(distances)))
-
-def assign_cell_types(max_x: int, max_y: int, regions: list, vertices: np.ndarray, cells: list, target_density: float) -> np.ndarray:
-    """
-    Assigns cell types based on the target density of multiciliated cells.
-
-    Args:
-        cells (list): List of cell data (x, y, neighbours, area).
-        target_density (float): Target density of multiciliated cells.
-
-    Returns:
-        np.ndarray: Array indicating the type of each cell (1 for multiciliated, 0 for basic, 3 for border).
-    """
-    type_mask = np.zeros(len(cells))
-    current_density = 0
-    
-    cell_points = np.array([np.array([cell[0], cell[1]]) for cell in cells])
-
-    accessed_points = []
-    for region in regions:
-        polygon = vertices[region]
-        for point in polygon:
-            border_index = is_center(polygon, cell_points)
-            accessed_points.append(border_index)
-            if (abs(0 - point[0]) < 0.00001) or (abs(max_x - point[0]) < 0.00001) or  (abs(0 - point[1]) < 0.00001) or (abs(max_y - point[1]) < 0.00001):
-                type_mask[border_index] = 3
-
-    # FIXME: This has absolutely terrible efficiency and is bottom-of-the-barrel stuff
-    for i in range(len(cell_points)):
-        if i not in accessed_points:
-            type_mask[i] = 3
-    
-    while current_density < target_density:
-        candidates = np.where(type_mask == 0)[0]
-        if len(candidates) == 0:
-            break
-
-        chosen_cell = np.random.choice(candidates)
-        type_mask[chosen_cell] = 1
-        for cell_index in cells[chosen_cell][2]:
-            if not type_mask[cell_index]:
-                type_mask[cell_index] = 2
-        current_density = len(np.where(type_mask == 1)[0]) / len(cells)
-
-    type_mask[np.where(type_mask == 2)[0]] = 0
-
-    return type_mask
-
-def calculate_intersection(a, b, c, d) -> np.ndarray:
-    x1, y1 = a
-    x2, y2 = b
-    x3, y3 = c
-    x4, y4 = d
-
-    denominator = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4)
-    if denominator == 0:
-        return np.array([])
-
-    t_numerator = (x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)
-    u_numerator = (x1 - x3) * (y1 - y2) - (y1 - y3) * (x1 - x2)
-    t = t_numerator / denominator
-    u = u_numerator / denominator
-
-    if 0 <= t <= 1 and 0 <= u <= 1:
-        intersection = a + t * (b - a)
-        return intersection
-    else:
-        return np.array([])
-
-def isin(a, b, threshold):
-    return np.where(np.all(np.abs(b - a) < threshold, axis=1))[0]
-    
-def constrain_voronoi(max_x: int, max_y: int, voronoi: Voronoi):
-    region_points = []
-    for region in voronoi.regions:
-        if not region or -1 in region:
-            continue
-        
-        new_region_points = []
-        for i in range(len(region)):
-            x, y = voronoi.vertices[region[i]]
-
-            if i < len(region)-1:
-                next_x, next_y = voronoi.vertices[region[i+1]]
-            else:
-                next_x, next_y = voronoi.vertices[region[0]]
-
-            flag = True
-
-            if (x < 0): # Left
-                if (0 <= next_y <= max_y) and (0 <= next_x <= max_x):
-                    intersection = calculate_intersection(np.array([x, y]), np.array([next_x, next_y]), [0, 0], [0, max_y])
-                    if intersection.size != 0:
-                        new_region_points.append(intersection)
-                        flag = False
-            elif (next_x < 0):
-                if (0 <= y <= max_y) and (0 <= x <= max_x):
-                    intersection = calculate_intersection(np.array([x, y]), np.array([next_x, next_y]), [0, 0], [0, max_y])
-                    new_region_points.append(np.array([x, y]))
-                    if intersection.size != 0:
-                        new_region_points.append(intersection)
-                        flag = False
-            elif (x > max_x): # Right
-                if (0 <= next_y <= max_y) and (0 <= next_x <= max_x):
-                    intersection = calculate_intersection(np.array([x, y]), np.array([next_x, next_y]), [max_x, 0], [max_x, max_y])
-                    if intersection.size != 0:
-                        new_region_points.append(intersection)
-                        flag = False
-            elif (next_x > max_x):
-                if (0 <= y <= max_y) and (0 <= x <= max_x):
-                    intersection = calculate_intersection(np.array([x, y]), np.array([next_x, next_y]), [max_x, 0], [max_x, max_y])
-                    new_region_points.append(np.array([x, y]))
-                    if intersection.size != 0:
-                        new_region_points.append(intersection)
-                        flag = False
-
-            if flag:
-                if (y < 0): # Down 
-                    if (0 <= next_y <= max_y) and (0 <= next_x <= max_x):
-                        intersection = calculate_intersection(np.array([x, y]), np.array([next_x, next_y]), [0, 0], [max_x, 0])
-                        if intersection.size != 0:
-                            new_region_points.append(intersection)
-                            flag = False
-                elif (next_y < 0):
-                    if (0 <= y <= max_y) and (0 <= x <= max_x):
-                        intersection = calculate_intersection(np.array([x, y]), np.array([next_x, next_y]), [0, 0], [max_x, 0])
-                        new_region_points.append(np.array([x, y]))
-                        if intersection.size != 0:
-                            new_region_points.append(intersection)
-                            flag = False
-                elif (y > max_y): # Up 
-                    if (0 <= next_y <= max_y) and (0 <= next_x <= max_x):
-                        intersection = calculate_intersection(np.array([x, y]), np.array([next_x, next_y]), [0, max_y], [max_x, max_y])
-                        if intersection.size != 0:
-                            new_region_points.append(intersection)
-                            flag = False
-                elif (next_y > max_y):
-                    if (0 <= y <= max_y) and (0 <= x <= max_x):
-                        intersection = calculate_intersection(np.array([x, y]), np.array([next_x, next_y]), [0, max_y], [max_x, max_y])
-                        new_region_points.append(np.array([x, y]))
-                        if intersection.size != 0:
-                            new_region_points.append(intersection)
-                            flag = False
-            
-            
-            if 0 <= x <= max_x and 0 <= y <= max_y and flag:
-                new_region_points.append(np.array([x, y]))
-
-        region_points.append(new_region_points)
-
-    # Consolidate points
-    constrained_vertices = np.empty((0, 2), dtype=float)
-    constrained_regions = []
-
-    for region in region_points:
-        region_map = []
-        for point in region:
-            if constrained_vertices.size > 0 :
-                vertex_index = isin(point, constrained_vertices, 0.00001)
-                if vertex_index.size == 0:
-                    constrained_vertices = np.append(constrained_vertices, [point], axis=0)
-                    region_map.append(len(constrained_vertices) - 1)
-                else:
-                    region_map.append(vertex_index[0])
-            else:
-                constrained_vertices = np.append(constrained_vertices, [point], axis=0)
-                region_map.append(len(constrained_vertices) - 1)
-
-        constrained_regions.append(region_map)
-
-    return (constrained_vertices, constrained_regions)
+            if iteration % 50 == 0:
+                plot = self.render(f"Tissue @ iteration {iteration}", plot)
 
 
 # TESTING
 tissue = Tissue(10, 10, 0.1)
+tissue.set_center_only(True)
 tissue.random_layout()
-tissue.render("Test Plot")
+tissue.anneal()
