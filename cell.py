a74a3a395882251ea0a3e06514b60526fb7b7d67
MetallicSquid <gcmacneil@icloud.com>
2024-07-10 21:07:29 +0100

    Boundaries are no longer constrained, area works, boundary points are 'ghosts'

--- cell.py
+++ cell.py
@@ -2,74 +2,24 @@ import numpy as np
 from functions import *
 
 class Cell():
-    def __init__(self, id: int, x: float, y: float, neighbours: np.ndarray, area: float):
+    def __init__(self, id: int, x: float, y: float, neighbours: np.ndarray):
         self.id = id
         self.x = x
         self.y = y
         self.neighbours = neighbours
-        self.area = area
-
-    def calculate_attraction(self, cells: list):
-        total_attraction = np.array([0.0, 0.0])
-        position = np.array([self.x, self.y])
-        for neighbour in self.neighbours:
-            neighbour_position = np.array([cells[neighbour].x, cells[neighbour].y])
-            difference = neighbour_position - position
-            distance = np.linalg.norm(difference)
-            unit_direction_vector = difference / distance
-           
-            # The simplest possible spring set up
-            force = distance
-            
-            total_attraction += force * unit_direction_vector
-
-        return total_attraction
-
-    def calculate_repulsion(self, cells: list):
-        total_repulsion = np.array([0.0, 0.0])
-        position = np.array([self.x, self.y])
-        neighbour_positions = np.array([np.array([cells[neighbour].x, cells[neighbour].y]) for neighbour in self.neighbours])
-        neighbourhood_area = polygon_area(neighbour_positions) 
-        area_difference = neighbourhood_area - self.area 
-
-        for neighbour in self.neighbours:
-            neighbour_position = np.array([cells[neighbour].x, cells[neighbour].y])
-            difference = neighbour_position - position
-            distance = np.linalg.norm(difference)
-            unit_direction_vector = difference / distance
-
-            force = area_difference
-
-            total_repulsion += force * unit_direction_vector
-
-        return total_repulsion
+        self.area = None
 
     def set_area(self, area: float):
         self.area = area
 
+    def step(self, force_matrix: np.ndarray, annealing: bool = False):
+        self.x, self.y = [self.x, self.y] + 0.95 * 0.01 * np.sum(force_matrix[self.id, self.neighbours], axis=0)
+
 class BasicCell(Cell):
-    def step(self, cells: list, annealing: bool = False):
-        if annealing:
-            net_force = self.calculate_attraction(cells)
-        else:
-            net_force = self.calculate_attraction(cells) - self.calculate_repulsion(cells)
-
-        self.x, self.y = [self.x, self.y] + 0.95 * 0.005 * net_force
-
+    pass
 
 class MulticiliatedCell(Cell):
-    def step(self, cells: list, annealing: bool = False):
-        if annealing:
-            net_force = self.calculate_attraction(cells)
-        else:
-            net_force = self.calculate_attraction(cells) - self.calculate_repulsion(cells)
-
-        self.x, self.y = [self.x, self.y] + 0.95 * 0.005 * net_force
-
-    def external_force(self, direction: np.ndarray, magnitude: float):
-        force = (direction * magnitude) / np.linalg.norm(direction) 
-        self.x, self.y = [self.x, self.y] + 0.95 * 0.005 * force
+    pass
 
 class BorderCell(Cell):
-    def step(self, cells: list, annealing: bool = False):
-        pass
+    pass
--- tissue.py
+++ tissue.py
@@ -6,6 +6,9 @@ from scipy.spatial import Voronoi, Delaunay
 from scipy.stats import qmc
 from collections import defaultdict
 import matplotlib.pyplot as plt
+
+import cProfile
+
 class Tissue():
     """
     Represents a tissue consisting of multiciliated and basic cells arranged in a 2D space.
@@ -38,6 +41,11 @@ class Tissue():
         self.center_only = False
         self.plot = (None, None)
 
+        # Exogenous flow and torque parameters
+        self.flow_direction = np.array([0, 0])
+        self.flow_magnitude = 0
+        self.torque_magnitude = 0
+
     def set_center_only(self, value: bool):
         self.center_only = value
 
@@ -50,10 +58,6 @@ class Tissue():
         """
         voronoi = Voronoi(points)
         neighbours = defaultdict(list)
-        areas = defaultdict(float)
-
-        # Constrain to tissue size
-        constrained_vertices, constrained_regions = constrain_voronoi(self.x, self.y, voronoi)
 
         # Calculate neighbours
         for ridge_point in voronoi.ridge_points:
@@ -63,31 +67,9 @@ class Tissue():
             if ridge_point[0] not in neighbours[ridge_point[1]]:
                 neighbours[ridge_point[1]].append(ridge_point[0])
 
-        # Calculate areas
-        for point_index, region_index in enumerate(voronoi.point_region):
-            region = voronoi.regions[region_index]
-            if -1 not in region:
-                polygon = constrained_vertices[region]
-                area = polygon_area(polygon)
-                areas[point_index] = area
-            else:
-                areas[point_index] = np.inf
-
-        cell_data = []
         for i in range(len(points)):
-            cell_data.append((points[i][0], points[i][1], np.array(neighbours[i]), areas[i]))
+            self.cells.append(BasicCell(i, points[i][0], points[i][1], np.array(neighbours[i])))
             
-        cell_type_mask = assign_cell_types(self.x, self.y, constrained_regions, constrained_vertices, cell_data, self.density, self.center_only)
-        
-        for i in range(len(cell_data)):
-            if cell_type_mask[i] == 1:
-                self.cells.append(MulticiliatedCell(i, *cell_data[i]))
-            elif cell_type_mask[i] == 3:
-                self.cells.append(BorderCell(i, *cell_data[i]))
-            else:
-                self.cells.append(BasicCell(i, *cell_data[i]))
-        
-        #self.cells = np.array(self.cells)
     
     def random_layout(self):
         """
@@ -102,6 +84,7 @@ class Tissue():
             points += 0.5
 
         points = np.array(points)
+
         self.generate_cells(points)
 
     def hexagonal_grid_layout(self):
@@ -157,89 +140,253 @@ class Tissue():
             fig, ax = plt.subplots()
         else:
             fig, ax = plot
-            ax.clear()
-
-        constrained_vertices, constrained_regions = constrain_voronoi(self.x, self.y, voronoi)
 
         # Plot cell edges
-        for i in range(len(constrained_regions)):
-            polygon = constrained_vertices[constrained_regions[i]]
-            ax.fill(*zip(*polygon), edgecolor="black", fill=False)
+        if len(border_points) == 0:
+            for i in range(len(voronoi.regions)):
+                if -1 not in voronoi.regions[i]:
+                    polygon = voronoi.vertices[voronoi.regions[i]]
+                    ax.fill(*zip(*polygon), edgecolor="black", fill=False)
+        else:
+            for i in range(len(self.cells)):
+                if not isinstance(self.cells[i], BorderCell):
+                    region_index = voronoi.point_region[i]
+                    polygon = voronoi.vertices[voronoi.regions[region_index]]
+                    ax.fill(*zip(*polygon), edgecolor="black", fill=False)
+
             
         # Plot springs
-        #for ridge in voronoi.ridge_points:
-        #    start_point = points[ridge[0]]
-        #    end_point = points[ridge[1]]
-        #    ax.plot([start_point[0], end_point[0]], [start_point[1], end_point[1]], color="blue")
+        #for i in range(len(points)):
+        #    start = points[i]
+        #    for neighbour_index in self.cells[i].neighbours:
+        #        end = points[neighbour_index]
+        #        ax.plot([start[0], end[0]], [start[1], end[1]], color="blue")
 
         # Plot center points
         basic_points = np.array(basic_points)
         multiciliated_points = np.array(multiciliated_points)
         border_points = np.array(border_points)
 
-        #ax.scatter(basic_points[:, 0], basic_points[:, 1], s=20, color="blue")
-        ax.scatter(multiciliated_points[:, 0], multiciliated_points[:, 1], s=20, color="orange")   
-        ax.scatter(border_points[:, 0], border_points[:, 1], s=20, color="green")   
+        ax.scatter(basic_points[:, 0], basic_points[:, 1], s=20, color="blue")
+        if multiciliated_points.size > 0 and border_points.size > 0:
+            ax.scatter(multiciliated_points[:, 0], multiciliated_points[:, 1], s=20, color="orange")   
+            ax.scatter(border_points[:, 0], border_points[:, 1], s=20, color="green")   
 
         # Plot border
         ax.plot([0, self.x, self.x, 0, 0], [0, 0, self.y, self.y, 0], 'k-')
-
         plt.title(title)
         plt.show()
-        plt.pause(0.1)
+
+        plt.xlim([0, self.x])
+        plt.ylim([0, self.y])
+
+        plt.pause(0.2)
+        
+        ax.clear()
 
         return (fig, ax)
 
-    def reevaluate_neighbours(self):
+    def set_cell_types(self):
+        type_mask = np.zeros(len(self.cells))
+        cell_points = np.array([np.array([cell.x, cell.y]) for cell in self.cells])
+        voronoi = Voronoi(cell_points)
+
+        for i in range(len(cell_points)):
+            region_index = voronoi.point_region[i]
+            if -1 in voronoi.regions[region_index]:
+                type_mask[i] = 1
+
+        if self.center_only:
+            area_center = np.array([self.x / 2, self.y / 2])
+            center_distances = []
+            for cell_point in cell_points:
+                center_distances.append(np.sum((area_center - cell_point) ** 2))
+
+            type_mask[int(np.argmin(np.array(center_distances)))] = 2
+        else:
+            current_density = 0
+            while current_density < self.density:
+                candidates = np.where(type_mask == 0)[0]
+                if len(candidates) == 0:
+                    break
+
+                chosen_cell = np.random.choice(candidates)
+                type_mask[chosen_cell] = 2
+                for cell_index in self.cells[chosen_cell].neighbours:
+                    if not type_mask[cell_index]:
+                        type_mask[cell_index] = 3
+
+                current_density = len(np.where(type_mask == 2)[0]) / len(self.cells)
+
+            type_mask[np.where(type_mask == 3)[0]] = 0
+
+        target_area = 0
+        count = 0
+        for i in range(len(cell_points)):
+            region_id = voronoi.point_region[i]
+            if -1 not in voronoi.regions[region_id]:
+                target_area += polygon_area(cell_points[self.cells[i].neighbours])
+                count += 1
+
+        target_area = target_area / count
+
+        for i in range(len(type_mask)):
+            id = self.cells[i].id
+            x = self.cells[i].x
+            y = self.cells[i].y
+            neighbours = self.cells[i].neighbours
+            if type_mask[i] == 0:
+                #self.cells[i].set_area(polygon_area(voronoi.vertices[voronoi.regions[voronoi.point_region[id]]]))
+                self.cells[i].set_area(target_area)
+            elif type_mask[i] == 1:
+                self.cells[i] = BorderCell(id, x, y, neighbours)
+            elif type_mask[i] == 2:
+                self.cells[i] = MulticiliatedCell(id, x, y, neighbours)
+                #self.cells[i].set_area(polygon_area(voronoi.vertices[voronoi.regions[voronoi.point_region[id]]]))
+                self.cells[i].set_area(target_area)
+
+    def set_flow(self, flow_direction, flow_magnitude):
+        self.flow_direction = flow_direction
+        self.flow_magnitude = flow_magnitude
+
+    def set_torque(self, torque_magnitude):
+        self.torque_magnitude = torque_magnitude
+
+    def calculate_force_matrix(self, annealing = False):
         cell_points = np.array([np.array([cell.x, cell.y]) for cell in self.cells])
         delaunay = Delaunay(cell_points)
+
         neighbour_vertices = delaunay.vertex_neighbor_vertices
-        
-        for i in range(len(cell_points)):
+
+        boundary_cells = []
+        for i in range(len(self.cells)):
             neighbours = neighbour_vertices[1][neighbour_vertices[0][i]:neighbour_vertices[0][i+1]]
-            self.cells[i].neighbours = np.array(neighbours)
+            self.cells[i].neighbours = neighbours
+            if isinstance(self.cells[i], BorderCell):
+                boundary_cells.append(i)
+
+        for cell in boundary_cells:
+            cell_neighbours = self.cells[cell].neighbours
+            boundary_neighbours = np.intersect1d(cell_neighbours, boundary_cells)
+            differences = cell_points[boundary_neighbours] - cell_points[cell]
+            distances = np.linalg.norm(differences, axis=1)
+            sorted_boundary_neighbours = boundary_neighbours[np.argsort(distances)]
+            furthest_neighbours = sorted_boundary_neighbours[2:]
+
+            for neighbour in furthest_neighbours:
+                self.cells[cell].neighbours = np.setdiff1d(self.cells[cell].neighbours, neighbour)
+                self.cells[neighbour].neighbours = np.setdiff1d(self.cells[neighbour].neighbours, cell)
+        
+        force_matrix = np.zeros((len(self.cells), len(self.cells), 2))
+
+        for i in range(len(cell_points)):
+            # Calculate attractive forces
+            neighbour_positions = cell_points[self.cells[i].neighbours]
+            differences = neighbour_positions - cell_points[i]
+            distances = np.linalg.norm(differences, axis=1)
+            unit_vectors = differences / distances[:, np.newaxis]
+
+            if isinstance(self.cells[i], BorderCell):
+                forces = ((distances[:, np.newaxis] - 1) * unit_vectors) / 10
+            else:
+                forces = (distances[:, np.newaxis] - 1) * unit_vectors
+
+            if not annealing:
+                # Calculate repulsive forces
+                if not isinstance(self.cells[i], BorderCell):
+                    area = polygon_area(cell_points[self.cells[i].neighbours])
+                    area_difference = self.cells[i].area - area
+
+                    forces -= (area_difference / len(unit_vectors)) * unit_vectors
+
+                if isinstance(self.cells[i], MulticiliatedCell):
+                    # Calculate external force contributions
+                    flow = self.flow_direction * self.flow_magnitude
+                    
+                    pinv_differences = np.linalg.pinv(differences.T)
+                    force_distribution = np.dot(pinv_differences, flow)
+
+                    external_forces = force_distribution[:, np.newaxis] * differences
+                    forces += external_forces 
+
+            force_matrix[i, self.cells[i].neighbours] += forces 
+
+        return force_matrix 
+
+    def evaluate_boundary(self):
+        # Determine which cells are boundary cells and the edges between them
+        boundary_cells = []
+        edges = []
+        for i in range(len(self.cells)):
+            if isinstance(self.cells[i], BorderCell):
+                for neighbour in self.cells[i].neighbours:
+                    if isinstance(self.cells[neighbour], BorderCell) and neighbour not in boundary_cells:
+                        edges.append((i, neighbour))
+                
+                boundary_cells.append(i)
+
+        # Add additional boundary cells
+        for edge in edges:
+            shared_cell = set(self.cells[edge[0]].neighbours).intersection(set(self.cells[edge[1]].neighbours))
+            shared_cell = shared_cell.difference(boundary_cells)
+            if len(shared_cell) == 0:
+                continue
+            shared_cell = shared_cell.pop()
+
+            a = np.array([self.cells[shared_cell].x, self.cells[shared_cell].y])
+            b = np.array([self.cells[edge[0]].x, self.cells[edge[0]].y])
+            c = np.array([self.cells[edge[1]].x, self.cells[edge[1]].y])
+            ab = a - b
+            ac = a - c
+            ab_dot_ac = np.dot(ab, ac)
+            angle = np.arccos(ab_dot_ac / (np.linalg.norm(ab) * np.linalg.norm(ac)))
+            
+            if angle > np.pi / 2:
+                edge_vector = c - b
+                edge_unit_vector = edge_vector / np.linalg.norm(edge_vector)
+
+                projection_length = np.dot(a - b, edge_unit_vector)
+                projection_vector = projection_length * edge_unit_vector
+
+                reflected_point = 2 * (b + projection_vector) - a
+
+                self.cells.append(BorderCell(len(self.cells), reflected_point[0], reflected_point[1], np.array([edge[0], edge[1], shared_cell])))
+                print(f"Adding new BorderCell at ({reflected_point[0]}, {reflected_point[1]})")
 
     def anneal(self, iterations: int = 2000):
         plt.ion()
         for iteration in range(iterations):
-            for cell in self.cells:
-                cell.step(self.cells, annealing=True)
+            force_matrix = self.calculate_force_matrix(annealing=True)
+            for i in range(len(self.cells)):
+                self.cells[i].step(force_matrix, annealing=True)
 
-            if iteration % 50 == 0:
-                self.reevaluate_neighbours()
+            if iteration % 100 == 0:
                 self.plot = self.render(f"Tissue annealing @ iteration {iteration}", self.plot)
 
-        # Set the areas of the cell neighbourhood polygons once annealing is complete
-        voronoi = Voronoi(np.array([np.array([cell.x, cell.y]) for cell in self.cells]))
-        constrained_vertices, _ = constrain_voronoi(self.x, self.y, voronoi)
+        self.set_cell_types()
+        self.evaluate_boundary()
 
-        for point_index, region_index in enumerate(voronoi.point_region):
-            region = voronoi.regions[region_index]
-            if -1 not in region:
-                polygon = np.array([np.array([self.cells[neighbour].x, self.cells[neighbour].y]) for neighbour in self.cells[point_index].neighbours])
-                area = polygon_area(polygon)
-                self.cells[point_index].set_area(area)
-            else:
-                self.cells[point_index].set_area(np.inf)
-
-
-    def exogenous_flow(self, flow_direction: np.ndarray, flow_magnitude: float, iterations: int = 5000):
-        plt.ion()
+    def simulate(self, iterations: int = 5000):
         for iteration in range(iterations):
+            force_matrix = self.calculate_force_matrix()
             for cell in self.cells:
-                cell.step(self.cells)
+                cell.step(force_matrix)
 
-                if isinstance(cell, MulticiliatedCell):
-                    cell.external_force(flow_direction, flow_magnitude)
+            self.evaluate_boundary()
 
-            if iteration % 50 == 0:
-                self.plot = self.render(f"Tissue under exogenous flow ({flow_magnitude}) @ iteration {iteration}", self.plot)
-        
+            if iteration % 100 == 0:
+                self.plot = self.render(f"Tissue under exogenous flow ({self.flow_magnitude}) @ iteration {iteration}", self.plot)
 
 
 # TESTING
-tissue = Tissue(10, 10, 0.1)
-tissue.set_center_only(True)
-tissue.random_layout()
-tissue.anneal()
-tissue.exogenous_flow(np.array([0, 1]), 5)
+def main():
+    tissue = Tissue(10, 10,  0.1)
+    tissue.set_center_only(True)
+    tissue.random_layout()
+    tissue.anneal()
+    tissue.set_flow(np.array([0, 1]), 0.4)
+    tissue.simulate(3000)
+
+
+cProfile.run('main()', sort="tottime")
+#main()
